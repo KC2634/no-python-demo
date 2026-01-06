5257de07f73be9dab6933eba591b5461b90ecde1
Keeley <clough.kee@nulondon.ac.uk>
2026-01-06 12:52:17 +0000

    adds function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello World")
+def my_function(user_answer):
+    if user_answer == 4:
+        print("that is correct!")
+    else:
+        print("that is incorrect")
+
+terminal_answer = int(input("what is 2 plus 2"))
+
+my_function(terminal_answer)
