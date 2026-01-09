6752045497575f603be0a9bf655adc22e4e1c0c3
Keeley <clough.kee@nulondon.ac.uk>
2026-01-09 17:00:53 +0000

    final quiz

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("Welcome to this quiz")
+print("Welcome to this quiz") #start of the quiz
 print("You are going to be given a set of questions, for each one answer with the value you think is correct")
-print("At the end of the quiz you will be given a percenatge score")
 print("good luck")
-def my_function(user_answer):
+
+#question 1
+def my_function(user_answer): #if functions to determine if answer is correct
     if user_answer == 4:
         print("that is correct!")
     else:
@@ -12,6 +13,12 @@ terminal_answer = int(input("what is 2 plus 2"))
 
 my_function(terminal_answer)
 
+#creating fucntion to calculate total marks at end of quiz
+
+if my_function == "that is correct!":
+    terminal_answer = 1
+
+#question 2
 def my_function2(user_answers):
     if user_answers == 10:
         print("correct")
@@ -21,8 +28,7 @@ def my_function2(user_answers):
 terminal_answer = int(input("what is 5 multiplied by 2?"))
 my_function2(terminal_answer)
 
-
-
+#question 3
 def my_function3(user_answers):
     if user_answers == 81:
         print("correct")
@@ -32,3 +38,5 @@ def my_function3(user_answers):
 terminal_answer = int(input("what is 9 squared?"))
 my_function3(terminal_answer)
 
+#end of quiz
+print ("well done thank you for taking this quiz")
